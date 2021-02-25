ca7558069ac934908923d30129a89e230922c1c8
hyeonseunglee <hyeonseung.lee@utexas.edu>
2021-02-25 04:52:06 -0600

    README fix

--- homework01/generate_animals.py
+++ homework01/generate_animals.py
@@ -8,7 +8,7 @@ heads = ['snake','bull','lion','raven','bunny']
 data['animals'] = []
 
 for x in range(0,20):
-	data['animals'].append({'head':random.choice(heads),'body':petname.Name()+'-'+petname.Name(),'arms':random.randint(2,10),'legs':random.randint(3,12),'tail':0})
+	data['animals'].append({'head':random.choice(heads),'body':petname.Name()+'-'+petname.Name(),'arms':random.randrange(2,11,2),'legs':random.randrange(3,13,3),'tail':0})
 	data['animals'][x]['tail'] = data['animals'][x]['arms']+data['animals'][x]['legs']
 with open('animals.json','w') as out:
 	json.dump(data,out,indent=2) 
